f7dc9bf04a9202abb3d9ef0a0bb0624546354082
Roque Barbosa <roquecostajr@outlook.com>
2022-10-07 21:51:36 -0300

    refactor: change duration to float

--- db/migrate/20221007022723_create_lectures.rb
+++ db/migrate/20221007022723_create_lectures.rb
@@ -2,7 +2,7 @@ class CreateLectures < ActiveRecord::Migration[7.0]
   def change
     create_table :lectures do |t|
       t.string :name
-      t.string :duration
+      t.float :duration
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_07_022723) do
 
   create_table "lectures", force: :cascade do |t|
     t.string "name"
-    t.string "duration"
+    t.float "duration"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
